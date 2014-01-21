24da513457e25bd7252259d71b678a0d2c757315
Anton Shemerey <shemerey@gmail.com>
2014-01-21 00:00:18 -0600

    stub user input properly

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
+class UserInput
+  def initialize(commands = nil)
+    @commands = Array(commands)
+  end
+
+  def chomp
+    @commands.shift || 'exit'
+  end
+end
+
 describe TicTacToe do
+  let(:input) { UserInput.new('Anton') }
+  before { TicTacToe.any_instance.stub(:gets).and_return(input) }
+
   it 'should ask about your name' do
-    TicTacToe.any_instance.stub(:start_game)
     capture{ subject.should be_instance_of(TicTacToe) }.should =~ /What is your name/
   end
 end
