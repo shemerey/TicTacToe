d7117e9e3b02539f94fca900f9a2faeaa643e1a6
Anton Shemerey <shemerey@gmail.com>
2014-01-23 17:46:09 -0600

    Advance use for respond_to rspec helper

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -34,12 +34,9 @@ describe TicTacToe do
 end
 
 describe TicTacToe::User do
-  it { should respond_to(:sign) }
-  it { should respond_to(:sign=) }
-  it { should respond_to(:name) }
-  it { should respond_to(:name=) }
-  it { should respond_to(:score) }
-  it { should respond_to(:score=) }
+  it { should respond_to(:sign, :sign=) }
+  it { should respond_to(:name, :name=) }
+  it { should respond_to(:score, :score=) }
 
   it 'should have zero socre by default' do
     subject.score.should be_zero
