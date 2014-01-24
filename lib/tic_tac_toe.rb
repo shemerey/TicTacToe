176739fbfbb302ea06dae7ad4e081dfd5cddd473
Anton Shemerey <shemerey@gmail.com>
2014-01-23 19:11:22 -0600

    extract user input into self value object

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,6 +117,27 @@ class TicTacToe
     end
   end
 
+  class User::Input
+    attr_reader :command, :column, :line
+
+    def initialize(input = '')
+      @command = input.chomp.downcase
+      @column, @line = command.split("")
+    end
+
+    def valid?
+      turn? || exit?
+    end
+
+    def turn?
+      ['a','b','c'].include?(column) && ['1','2','3'].include?(line)
+    end
+
+    def exit?
+      :exit == command.to_sym
+    end
+  end
+
   def user_turn
     put_line
     puts "\n  RUBY TIC TAC TOE".purple
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -60,6 +60,65 @@ describe TicTacToe::User do
   end
 end
 
+describe TicTacToe::User::Input do
+  it { should respond_to(:turn?, :exit?) }
+
+  it 'expty input should not be turn' do
+    should_not be_turn
+  end
+
+  describe '#column, #line' do
+    it 'should split user command properly' do
+      described_class.new('a1').column.should == 'a'
+      described_class.new('a1').line.should == '1'
+    end
+  end
+
+  context '#valid?' do
+    it 'exit is a valid input' do
+      described_class.new('exit').should be_valid
+    end
+
+    it 'turn is a valid input' do
+      described_class.new('a1').should be_valid
+    end
+
+    it "shouild be invalid if input doesn't match turn or exit" do
+      described_class.new('bla-bla-bla').should_not be_valid
+    end
+  end
+
+  context '#turn?' do
+    let(:turns) { %w[a1 a2 a3 b1 b2 b3 c1 c2 c3] }
+
+    it "valid turns" do
+      turns.each do |turn|
+        described_class.new(turn).should be_turn
+      end
+    end
+
+    it 'aa or 11, not a turn' do
+      described_class.new('11').should_not be_turn
+      described_class.new('aa').should_not be_turn
+    end
+
+    it 'exit not a turn' do
+      described_class.new('exit').should_not be_turn
+    end
+  end
+
+  context '#exit?' do
+    subject { described_class.new('exit') }
+    it '#exit?' do
+      subject.should be_exit
+    end
+
+    it 'should not be valid input' do
+      subject.should_not be_turn
+    end
+  end
+end
+
 describe TicTacToe::Board do
   it { should respond_to(:draw) }
 
